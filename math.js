c3f4d60437cc08b183ed0ca3a94aaa143c0ca7e9
Petar Dosev <pdosev@gmail.com>
2022-10-22 10:17:06 -0400

    sine cosine so fine

--- math.js
+++ math.js
@@ -19,29 +19,77 @@ function init() {
 function draw() {
   helper.context.clearRect(0, 0, helper.element.width, helper.element.height);
   drawCircle(helper.context);
-  drawLine(helper.context);
+  drawLines(helper.context);
 }
 
-function drawLine(ctx) {
-  let pointerX = helper.radius * Math.sin(helper.pointerAngle);
-  let pointerY = helper.radius * Math.cos(helper.pointerAngle);
+function drawLines(ctx) {
   ctx.beginPath();
+  ctx.lineStyle = "black";
+  ctx.lineWidth = 1;
+  ctx.moveTo(helper.circleX, helper.circleY - helper.radius - 20);
+  ctx.lineTo(helper.circleX, helper.circleY + helper.radius + 20);
+  ctx.moveTo(helper.circleX - helper.radius - 20, helper.circleY);
+  ctx.lineTo(helper.circleX + helper.radius + 20, helper.circleY);
+  ctx.stroke();
+  ctx.closePath();
+
+  let pointerSine = Math.sin(helper.pointerAngle);
+  let pointerCosine = Math.cos(helper.pointerAngle);
+  let pointerX = helper.radius * pointerSine;
+  let pointerY = helper.radius * pointerCosine;
+  ctx.beginPath();
+  ctx.lineStyle = "black";
+  ctx.fillStyle = "black";
   ctx.moveTo(helper.circleX, helper.circleY);
   ctx.lineTo(helper.circleX + pointerX, helper.circleY + pointerY);
   ctx.stroke();
+  drawLabeledLine(ctx, helper.circleX,
+                  helper.circleY,
+                  helper.circleX + pointerX,
+                  helper.circleY,
+                  "darkblue",
+                  -15,
+                  roundUp(pointerCosine, 3)
+                  );
+   drawLabeledLine(ctx, helper.circleX + pointerX,
+                  helper.circleY,
+                  helper.circleX + pointerX,
+                  helper.circleY + pointerY,
+                  "darkred",
+                  -15,
+                  roundUp(pointerSine, 3)
+                  );
+}
+
+function drawLabeledLine(ctx, startX, startY, endX, endY, color, textOffset, text) {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(endX, endY);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.font = "20px Arial";
+  ctx.miterLimit = 2;
+  ctx.lineJoin = "circle";
+  ctx.lineWidth = 1;
+  ctx.strokeText(text, endX / 1.5, endY + textOffset);
+  //ctx.fillText(text, startX + (endX / 2), startY + (endY / 2) + textOffset);
 }
 
 function drawCircle(ctx) {
-  ctx.lineStyle = "black";
   ctx.lineWidth = 2;
   ctx.beginPath();
+  ctx.lineStyle = "black";
+  ctx.fillStyle = "black";
   ctx.arc(helper.circleX, helper.circleY, helper.radius, 0, 2*Math.PI);
   ctx.stroke();
 }
 
-function update(event) {
-  event.x;
-  event.y;
+function roundUp(num, precision) {
+  precision = Math.pow(10, precision)
+  return Math.ceil(num * precision) / precision
 }
+
 
 init()
